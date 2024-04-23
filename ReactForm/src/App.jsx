560d451fc015bc17bac6cfce8bbffe1489979114
Ayyushiiii <ks742225@gmail.com>
2024-04-24 00:30:51 +0530

    Signup for jsx ready

---
+++ ReactForm/src/App.jsx
@@ -0,0 +1,13 @@
+import LoginSignup from "./components/LoginSignup/LoginSignup";
+
+function App() {
+  return (
+    <>
+      <div>
+        <LoginSignup></LoginSignup>
+      </div>
+    </>
+  );
+}
+
+export default App;

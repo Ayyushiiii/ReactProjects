0aa0be9eaf43553298544d4807e1bb0fbc4eef86
Ayyushiiii <ks742225@gmail.com>
2024-04-25 01:10:44 +0530

    EduWeb upload

--- EduWeb/src/components/Navbar/Navbar.jsx
+++ EduWeb/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./Navbar.css";
 import logo from "../../assets/logo.png";
-import { Link } from "react-scroll";
+import { Link, ScrollLink } from "react-scroll";
 import menu_icon from "../../assets/menu-icon.png";
 
 const Navbar = () => {

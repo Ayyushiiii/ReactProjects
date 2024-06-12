6b838d038fb69066c839a77a2c78aab4cbecf1e9
Ayyushiiii <ks742225@gmail.com>
2024-06-12 13:55:46 +0530

    mega blog

---
+++ megaBlog/src/components/AuthLayout.jsx
@@ -0,0 +1,22 @@
+import React,{useEffect, useState} from 'react';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import authService from '../appwrite/auth';
+
+
+export default function Protected({children, authentication= true}) {
+
+    const navigate = useNavigate();
+    const [loader, setLoader] = useState(true);
+    const authStatus = useSelector(state => state.auth.status)
+    useEffect(()=>{
+        if(authentication && authService !== authentication){
+            navigate("/login")
+        }
+        else if(!authentication && authService !== authentication){
+            navigate("/")
+        }
+    },[authStatus, navigate, authentication])
+   return loader ? <h1>Loading...</h1> : <>{children}</>
+}
+
---
+++ megaBlog/src/components/index.js
@@ -0,0 +1,30 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from './container/Container';
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import RTE from './RTE';
+import Signup from "./Signup";
+import Login from './Login';
+import PostForm from './post-form/PostForm';
+import PostCard from "./PostCard";
+import AuthLayout from './AuthLayout';
+import Input from '../components/Input';
+import Select from '../components/Select';
+import Button from '../components/Button'
+
+export{Header,
+       Footer,
+       Container,
+       Logo,
+       LogoutBtn,
+       RTE,
+       Signup,
+       Login,
+       PostForm,
+       PostCard,
+       AuthLayout,
+       Input,
+       Select,
+       Button
+}
